3bd8e8449f2f41b1bd6be4e3a03ff79ae2b2c13d
maloslov <60790985+maloslov@users.noreply.github.com>
2021-02-20 23:52:23 +0300

    BaseEditor update

--- AVBaseEditor/App.xaml.cs
+++ AVBaseEditor/App.xaml.cs
@@ -13,44 +13,28 @@ namespace AVBaseEditor
     /// </summary>
     public partial class App : Application
     {
-        public static List<Phone> Phones { get; set; }
     }
     public class BaseRecord
     {
         public string VirusName { get; set; }
         public string FileType { get; set; }
-        public UInt32 Signature { get; set; }
+        public byte Signature { get; set; }
         public UInt32 Length { get; set; }
         public UInt32 Hash { get; set; }
         public UInt32 OffsetStart { get; set; }
         public UInt32 OffsetEnd { get; set; }
-        public BaseRecord(string VirusName, string FileType, 
-            UInt32 Signature, UInt32 Length, UInt32 Hash, 
+        public BaseRecord(string VirusName, string FileType,
+            byte Signature, UInt32 Length, UInt32 Hash,
             UInt32 OffsetStart, UInt32 OffsetEnd)
         {
             this.VirusName = VirusName;
             this.FileType = FileType;
             this.Signature = Signature;
             this.Length = Length;
+            System.Security.Cryptography.SHA256 sha;
             this.Hash = Hash;
             this.OffsetStart = OffsetStart;
             this.OffsetEnd = OffsetEnd;
-        }
-    }
-    public class Phone
-    {
-        public string Title { get; set; }
-        public string Company { get; set; }
-        public int Price { get; set; }
-        public Phone(string Title, string Company, int Price)
-        {
-            this.Title = Title;
-            this.Company = Company;
-            this.Price = Price;
-        }
-        public override string ToString()
-        {
-            return Title + ", " + Company + ", " + Price.ToString();
         }
     }
 }
--- AVBaseEditor/MainWindow.xaml.cs
+++ AVBaseEditor/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,30 +23,31 @@ namespace AVBaseEditor
     public partial class MainWindow : Window
     {
         private List<BaseRecord> Base { get; set; }
-        private string BasePath { get; set; }
+        private string BaseDirectory { get; set; }
+        private const string Sign = "basov";
+        private const UInt32 recordCount = 512;
 
         //public List<Phone> Phones { get; set; }
         public MainWindow()
         {
             InitializeComponent();
-            App.Phones = new List<Phone>();
             Base = new List<BaseRecord>();
             dataGrid.ItemsSource = Base;
             //Test Example
-            Base.Add(new BaseRecord("Petya", "exe", 10101010, 25, 5465656, 1, 56));
-            Base.Add(new BaseRecord("Vanya", "txt", 11101010, 25, 5465321, 2, 16));
-            Base.Add(new BaseRecord("Sasha", "png", 01101011, 27, 5466235, 21, 26));
-            Base.Add(new BaseRecord("Misha", "mp3", 10111011, 27, 5456173, 11, 26));
-            Base.Add(new BaseRecord("Pasha", "mp4", 10111111, 29, 5656213, 12, 15));
-            Base.Add(new BaseRecord("Maxim", "pdf", 10110110, 28, 4656978, 14, 51));
+            Base.Add(new BaseRecord("Petya", "exe", 0b_1010_1010, 25, 5465656, 1, 56));
+            Base.Add(new BaseRecord("Vanya", "txt", 0b_1110_1010, 25, 5465321, 2, 16));
+            Base.Add(new BaseRecord("Sasha", "png", 0b_0110_1011, 27, 5466235, 21, 26));
+            Base.Add(new BaseRecord("Misha", "mp3", 0b_1011_1011, 27, 5456173, 11, 26));
+            Base.Add(new BaseRecord("Pasha", "mp4", 0b_1011_1111, 29, 5656213, 12, 15));
+            Base.Add(new BaseRecord("Maxim", "pdf", 0b_1011_0110, 28, 4656978, 14, 51));
         }
 
         private void MenuItemDelete_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show(dataGrid.CurrentItem.ToString());
-            BaseRecord data = (BaseRecord)dataGrid.CurrentItem;
-            Base.Remove(Base.Find(x => 
-                x.VirusName.Equals(data.VirusName)));
+            foreach(BaseRecord rec in dataGrid.SelectedItems)
+            {
+                Base.Remove(rec);
+            }
             dataGrid.Items.Refresh();
             
         }
@@ -53,26 +55,68 @@ namespace AVBaseEditor
         private void LoadBaseFromDirectory(object sender, RoutedEventArgs e)
         {
             
-            OpenFileDialog openFileDialog = new OpenFileDialog();
+            var openFileDialog = new OpenFileDialog();
             openFileDialog.DefaultExt = "directory";
             openFileDialog.CheckFileExists = false;
             openFileDialog.Filter = "Directory|Directory";
             openFileDialog.FileName = "Directory";
             if (openFileDialog.ShowDialog() == true)
             {
-                BasePath = openFileDialog.FileName.TrimEnd("Directory.directory".ToCharArray());
-
+                BaseDirectory = openFileDialog.FileName.Substring(0, 
+                    openFileDialog.FileName.LastIndexOf('\\')+1);
+                MessageBox.Show(BaseDirectory);
             }
         }
         private void SaveBaseToDirectory(object sender, RoutedEventArgs e)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            if (BaseDirectory == null)
+                LoadBaseFromDirectory(sender, e);
+
+            FileStream fs;
+            //check file
+            if (File.Exists(BaseDirectory + "base.avb"))
+            {
+                //check sign
+                fs = new FileStream(BaseDirectory + "base.avb", 
+                    FileMode.Open);
+                byte[] buf = new byte[Sign.Length+1];
+                fs.Read(buf, 0, buf.Length);
+                if (!Sign.Equals(btos(buf)))
+                {
+                    MessageBox.Show("Base file is broken!", "Warning", 
+                        MessageBoxButton.OK, MessageBoxImage.Warning);
+                    return;
+                }
+            }
+            else
+            {
+                //create & sign file
+                fs = new FileStream(BaseDirectory + "base.avb", 
+                    FileMode.CreateNew);
+                fs.Write(stob(Sign), 0, Sign.Length);
+                var co = BitConverter.GetBytes(512);
+                //fs.Write(co, BaseDirectory.Length, co.Length);
+            }
+        }
+        private string btos(byte[] bytes)
+        {
+            string res = "";
+            foreach (char c in bytes)
+                res += c;
+            return res;
+        }
+        private byte[] stob(string str)
+        {
+            byte[] res = new byte[str.Length+1];
+            for (int i = 0; i < str.Length; i++)
+            {
+                res[i] = (byte)str[i];
+            }
+            res[str.Length] = (byte)'\0';
+            return res;
         }
         private void AppendBaseRecord(object sender, RoutedEventArgs e)
-        {
-            throw new NotImplementedException();
-        }
-        private void DeleteRecordsInRange(object sender, RoutedEventArgs e)
         {
             throw new NotImplementedException();
         }
